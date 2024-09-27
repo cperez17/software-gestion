d107ed59113707c837d0cb20e80885e5dcc0f1df
XIMaxi <maxiarriagada.p@outlook.com>
2024-09-27 20:55:58 -0300

    elimar paginas y cambios pagina principal

--- gestion-app/app/page.tsx
+++ gestion-app/app/page.tsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import styles from './styles/Main.module.css';
 
-export default function Home(){
-  return(
-    <section className="py-24">
-      <div className="container">
-        <h1 className="text-3x1 font-bold">TEST</h1>
+const mainPage = () => {
+  return (
+    <div className={styles.container}>
+      {/* Barra Superior */}
+      <div className={styles.topBar}>
+        <p > Administrador </p>
       </div>
-    </section>
-  )
-}
+
+      {/* Barra de Navegación */}
+      <nav className={styles.navbar}>
+        <div className={styles.navContent}>
+          {/* Logo en la esquina izquierda */}
+          {/*<div className={styles.logo}>
+            <Image
+              src="/img/LogoICOC.png" // Ruta desde la carpeta public
+              alt="Logo"
+              width={150} // Ajusta según sea necesario
+              height={50} // Ajusta según sea necesario
+              className={styles.logoImage}
+            />
+          </div>*/}
+
+          {/* Enlaces de Navegación */}
+          <ul className={styles.navLinks}>
+          </ul>
+        </div>
+      </nav>
+      <div>
+              {/* Barra de Título */}
+      <div className={styles.titleBar}>
+        <h1>Gestión ICOC</h1>
+      </div>
+          <div className={styles.logo}>
+            <Image
+              src="/img/Logo Civil_PNG.webp" // Ruta desde la carpeta public
+              alt="Logo"
+              width={1000} // Ajusta según sea necesario
+              height={500} // Ajusta según sea necesario
+              className={styles.logoImage}
+            />
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default mainPage;
