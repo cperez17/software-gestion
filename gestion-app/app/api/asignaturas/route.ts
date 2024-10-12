712608f535588743b7afd7ed61b16e43f48568d6
Jerakovich <jeral122001@gmail.com>
2024-10-11 21:25:10 -0300

    agregue backend asignaturas y la bd

--- gestion-app/app/api/asignaturas/route.ts
+++ gestion-app/app/api/asignaturas/route.ts
@@ -1,11 +1,42 @@
 // app/api/asignaturas/route.ts
+
 import { NextResponse } from 'next/server';
+import { Pool } from 'pg';
 
+// Configuración de la conexión a PostgreSQL
+const pool = new Pool({
+  user: "postgres",
+  host: "db",
+  database: "soft-tech",
+  password: "jera123", 
+  port: 5432,
+});
+
+// Manejo de la solicitud GET
 export async function GET() {
-  const asignaturas = [
-    { id: 1, nombre: 'Matemáticas', creditos: 4 },
-    { id: 2, nombre: 'Física', creditos: 3 },
-  ];
+    try {
+      console.log("hola");
+        const res = await pool.query('SELECT * FROM courses');
+        return NextResponse.json(res.rows);
+    } catch (error) {
+        console.error('Error al obtener las asignaturas:', error);
+        return NextResponse.json({ error: 'Error al obtener las courses' }, { status: 500 });
+    }
+}
 
-  return NextResponse.json(asignaturas);
+// Manejo de la solicitud POST
+export async function POST(request: Request) {
+    const body = await request.json();
+    const { course_name, credits, semester_id, code } = body;
+
+    try {
+        const res = await pool.query(
+            'INSERT INTO courses (course_name, credits, semester_id, code) VALUES ($1, $2, $3, $4) RETURNING *',
+            [course_name, credits, semester_id, code]
+        );
+        return NextResponse.json(res.rows[0], { status: 201 });
+    } catch (error) {
+        console.error('Error al agregar la asignatura:', error);
+        return NextResponse.json({ error: 'Error al agregar la asignatura' }, { status: 500 });
+    }
 }
---
+++ gestion-app/app/api/db.js
@@ -0,0 +1,22 @@
+//docker-compose exec nextapp node app/api/db.js
+import pkg from 'pg'; // Importa el módulo pg como un paquete por defecto
+const { Client } = pkg; // Desestructura Client del paquete
+
+const client = new Client({
+  user: "postgres",
+  host: "db",
+  database: "soft-tech",
+  password: "jera123", 
+  port: 5432,
+});
+
+client.connect()
+  .then(async () => {
+    console.log("Conectado a la base de datos");
+    
+    // Consulta de prueba: selecciona todos los registros de una tabla
+  })
+  .catch(err => console.error("Error al conectar a la base de datos", err))
+  .finally(() => client.end()); 
+
+export default client;
